9317f990e9cf6698e09f62fd70539264fccc657e
Byspll7 <148060095+Byspll7@users.noreply.github.com>
2025-01-28 18:01:55 +0400

    get all commands

---
+++ AuthWebApi/AuthWebApi/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using AuthWebApi.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.General;
+
+namespace AuthWebApi.Data
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Blogs> Blogs { get; set; }
+
+    }
+}
